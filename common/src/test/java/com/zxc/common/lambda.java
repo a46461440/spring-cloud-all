8ff1999442961a57370bcbe7170d50a18df46d93
ZXC <496461440@qq.com>
2019-01-14 00:51:03 +0800

    lambda表达式测试

---
+++ common/src/test/java/com/zxc/common/lambda.java
@@ -0,0 +1,75 @@
+package com.zxc.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+/**
+ * 测试lambda
+ *
+ * @author Zhou RunMing
+ * @date 2019/1/13
+ */
+@Slf4j
+public class lambda {
+
+    public void test1(List<Integer> list, Predicate<Integer> predicate) {
+        list.stream().forEach(l -> {
+            if (predicate.test(l))
+                this.log.info("{}是大于5的", l);
+        });
+    }
+
+    @Test
+    public void testPredicate() {
+        List<Integer> list = Arrays.asList(1, 5, 6, 7, 8, 9, 10, 4, 2);
+        this.test1(list, l -> l > 5);
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class Customer {
+        private String name;
+        private Integer age;
+        private boolean sex;
+
+        public String print(Function<Customer, String> function) {
+            return function.apply(this);
+        }
+    }
+
+    @Test
+    public void testFunction() {
+        Customer customer = new Customer("zxc", 24, true);
+        String msg = customer.print(c -> String.format("name is %s,the age is %d and the sex is %s",
+                customer.getName(), customer.getAge(), customer.isSex() ? "male" : "female"));
+        this.log.info(msg);
+    }
+
+    public Customer supplierProvide(Supplier<Customer> supplier) {
+        return supplier.get();
+    }
+
+    @Test
+    public void testApplier() {
+        System.out.println(this.supplierProvide(() -> new Customer()) == this.supplierProvide(Customer::new));
+    }
+
+    @Test
+    public void testConsumer() {
+        Consumer<Customer> consumer = c -> this.log.info(c.toString());
+        Consumer<Customer> consumer2 = c -> this.log.info(c.toString() + "ok");
+        Customer customer = new Customer();
+        consumer.andThen(consumer2).accept(customer);
+    }
+}
